9f6012e2324608e7632b971ddf56648431930f3a
ripanrenaldi25 <ripanrenaldi25042002@gmail.com>
2026-01-08 12:10:58 +0700

    feat(hero): Create hero section and add scrolling  animation to it

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,14 @@
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars" });
     const paragrafSplit = new SplitText(".subtitle", { type: "lines" });
@@ -57,48 +63,84 @@ const Hero = () => {
       },
       0
     );
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    const videoTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    if (!videoRef.current) {
+      return;
+    } else {
+      videoRef.current.onloadedmetadata = () => {
+        videoTimeline.to(videoRef.current, {
+          currentTime: videoRef!.current!.duration,
+        });
+      };
+    }
   }, []);
 
   return (
-    <section className="noisy " id="hero">
-      <h1
-        className="title bg-linear-to-b from-black to-white 
+    <>
+      <section className="noisy " id="hero">
+        <h1
+          className="title bg-linear-to-b from-black to-white 
            bg-clip-text  text-transparent"
-      >
-        MOJITO
-      </h1>
+        >
+          MOJITO
+        </h1>
 
-      <img
-        src="/images/hero-left-leaf.png"
-        alt="left-leaf"
-        className="left-leaf"
-      />
+        <img
+          src="/images/hero-left-leaf.png"
+          alt="left-leaf"
+          className="left-leaf"
+        />
 
-      <img
-        src="/images/hero-right-leaf.png"
-        alt="left-leaf"
-        className="right-leaf"
-      />
+        <img
+          src="/images/hero-right-leaf.png"
+          alt="left-leaf"
+          className="right-leaf"
+        />
 
-      <div className="body">
-        <div className="content">
-          <div className="space-y-5 hidden md:block">
-            <p className="subtitle">Cool, Crisp, Classic</p>
-            <p className="subtitle">
-              Sip the spirit <br /> of summer
-            </p>
-          </div>
+        <div className="body">
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p className="subtitle">Cool, Crisp, Classic</p>
+              <p className="subtitle">
+                Sip the spirit <br /> of summer
+              </p>
+            </div>
 
-          <div className="view-cocktails">
-            <p className="subtitle">
-              Every cocktail on our menu is a blend of premium ingredients,
-              creative flair, and timeless recipes — designed to delight your
-              senses.
-            </p>
+            <div className="view-cocktails">
+              <p className="subtitle">
+                Every cocktail on our menu is a blend of premium ingredients,
+                creative flair, and timeless recipes — designed to delight your
+                senses.
+              </p>
+            </div>
           </div>
         </div>
+      </section>
+
+      <div className="video bg-red-500">
+        <video
+          src="/videos/output2.mp4"
+          playsInline
+          muted
+          className="w-full absolute object-bottom h-1/2 bottom-0 left-0"
+          preload="auto"
+          ref={videoRef}
+        />
       </div>
-    </section>
+    </>
   );
 };
 
